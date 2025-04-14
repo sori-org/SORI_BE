fd9e89ec3a771f1378075d36d72dafa17a1d10e6
Yim202035379 <yim0619@gachon.ac.kr>
2025-04-15 04:24:48 +0900

    ec2 설정

--- src/database/database.py
+++ src/database/database.py
@@ -12,12 +12,13 @@ def get_db():
     finally:
         db.close()
 # .env 파일 로드
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', '.env'))
+load_dotenv(dotenv_path=".env")
 
 # 환경변수에서 DB 설정 값 불러오기
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
+print("📌 DB_HOST:", DB_HOST)
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME")
 
