0e9515a7e2c67577ffee27773ac5608de01f4115
Yim202035379 <yim0619@gachon.ac.kr>
2025-04-18 22:53:31 +0900

    database->db

--- alembic/env.py
+++ alembic/env.py
@@ -8,12 +8,8 @@ from alembic import context
 
 # ✅ 1. Load .env for DB connection info
 load_dotenv()
-db_user = os.getenv("DB_USER")
-db_pass = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_HOST")
-db_port = os.getenv("DB_PORT")
-db_name = os.getenv("DB_NAME")
-database_url = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+
+database_url = "DATABASE_URL"
 
 # ✅ 2. Setup system path for module import
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
--- src/db/database.py
+++ src/db/database.py
@@ -14,16 +14,9 @@ def get_db():
 # .env 파일 로드
 load_dotenv(dotenv_path=".env")
 
-# 환경변수에서 DB 설정 값 불러오기
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-print("📌 DB_HOST:", DB_HOST)
-DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME")
 
 # MySQL 연결 URL 설정
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = "DATABASE_URL"
 
 # SQLAlchemy 구성
 engine = create_engine(DATABASE_URL)
