14e7f706344940f61d4de097fb01935449ed4290
Yim202035379 <yim0619@gachon.ac.kr>
2025-04-18 16:54:59 +0900

    로그인, 회원탈퇴 재 테스트

--- src/services/auth/kakao_unlink.py
+++ src/services/auth/kakao_unlink.py
@@ -1,9 +1,17 @@
 import requests
 
 def unlink_kakao_user(access_token: str):
-    response = requests.post(
-        "https://kapi.kakao.com/v1/user/unlink",
-        headers={"Authorization": f"Bearer {access_token}"}
-    )
-    if response.status_code != 200:
-        raise Exception(f"카카오 언링크 실패: {response.text}")
+    url = "https://kapi.kakao.com/v1/user/unlink"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8"
+    }
+
+    res = requests.post(url, headers=headers)
+    print("📡 언링크 응답 코드:", res.status_code)
+    print("📡 응답 본문:", res.text)
+
+    if res.status_code != 200:
+        raise Exception(f"카카오 언링크 실패: {res.text}")
+
+    return res.json()
