cdb08c8f9f7c8dbafa6d3b06e1ebc41a59f3d503
Odeng2 <151814873+Odeng2@users.noreply.github.com>
2025-04-20 17:57:39 +0900

    오류 수정 및 mypage api 추가

--- src/api/search.py
+++ src/api/search.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Query
 from src.services.naver_search import search_places
 from src.schemas.search_schema import PlaceSearchResponse
 
-router = APIRouter()
+router = APIRouter(tags=["search"])
 
-@router.get("/place", response_model=list[PlaceSearchResponse], tags=["search"])  # ✅ 여기에 직접
+@router.get("/place", response_model=list[PlaceSearchResponse])
 def search_store(search_keyword: str = Query(..., description="검색할 키워드")):
     return search_places(search_keyword)
