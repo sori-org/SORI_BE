aadcd7f80cc0734c7c367e0fba2dfc420cb25531
Odeng2 <151814873+Odeng2@users.noreply.github.com>
2025-04-20 15:39:43 +0900

    try2: resolve conflict

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,11 +1,10 @@
 from pydantic import BaseModel
-from typing import Optional
-
+from typing import Optional, List
+from src.schemas.stores import StoreOut
 
 class UserBase(BaseModel):
     display_name: str
     profile_image: Optional[str] = None
-    phone_number: Optional[str] = None
 
 
 class UserCreate(UserBase):
@@ -16,9 +15,11 @@ class UserOut(UserBase):
     user_id: int
     account_id: int
     main_store_id: Optional[int] = None
+    storeList: Optional[List[StoreOut]] = []
 
     class Config:
-        orm_mode = True
+        from_attributes = True
+
 
 class NicknameUpdate(BaseModel):
     nickname: str
