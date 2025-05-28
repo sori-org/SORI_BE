dbf0bacc69069e9651e547082da59ebc96d84134
윤승 <frogggggggy@gmail.com>
2025-05-28 14:29:52 +0900

    cors

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://sori-fe.vercel.app"
+        "https://sori-fe.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
